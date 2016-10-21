ab180db32a10f8efe8caf8fb9376854dc6dabaeb
Casey Alvarado <alvarado.casey@gmail.com>
2016-10-21 17:28:58 -0400

    working hold button

--- ArduinoSerialTestv2/ArduinoSerialTestv2.ino
+++ ArduinoSerialTestv2/ArduinoSerialTestv2.ino
@@ -10,37 +10,47 @@ bool previousState = HIGH;
 bool currentState; 
 
 //keeps track of the first time a button was pressed
-unsigned long firstTimePressed; 
+unsigned long firstTimePressed = 0; 
+
+int buttonPin = 7; 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
-  pinMode(7, INPUT);
+//  pinMode(5, INPUT);
+//  pinMode(6, INPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  val1 = digitalRead(3);
-  currentState = val1; 
-  if ((currentState == LOW) && (previousState == HIGH) && ((millis() - firstTimePressed)> 200)){ 
-    firstTimePressed = millis(); 
-  } 
+  val1 = digitalRead(buttonPin);
+  currentState = !val1; 
   
+  
+  if ((currentState == LOW) && (previousState == HIGH)){ 
+    Serial.print("FIRST TIME");
+    Serial.println(millis());
+    firstTimePressed = millis(); 
+    previousState = LOW; 
+    delay(200);
+  } 
+  Serial.println(firstTimePressed);
   //how long has this been held for? 
   long timeHeld = millis() - firstTimePressed; 
-  
+  Serial.println(timeHeld);
   //if the amount of time held for it greater than some value to counteract noise 
   //the value of 50 can be experimented with 
-  if (timeHeld > 50){ 
-    if (currentState = HIGH && previousState ==LOW){ 
+  if ((timeHeld > 50) && (firstTimePressed !=0)){ 
+    if (currentState == HIGH && previousState ==LOW){ 
       Serial.print("releasing state, held for");
       Serial.print(timeHeld);
       Serial.println(timeHeld/1000); 
+      previousState = currentState;
     } 
+
   }
-  previousState = currentState; 
-  
+  delay(200);
+//  
   
 //  val2 = digitalRead(5);
 //  val3 = digitalRead(10);
