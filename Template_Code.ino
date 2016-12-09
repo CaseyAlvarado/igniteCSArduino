af85e7cd808fd9bafcbcd810d4632c8554d132f7
apan64 <andrew.pan@students.olin.edu>
2016-12-09 16:19:32 -0500

    Template

---
+++ Template_Code.ino
@@ -0,0 +1,36 @@
+// Arduino Explorations: Mosignor Haddad Middle School
+
+
+// The next five lines are called "variable assignments"
+// which means they are defining the word on the left
+// to mean the value on the right.
+// "int" means the value is an integer (whole number).
+int LED1    = 2;
+int LED2 = 3;
+
+int leftButton = 5;
+int middleButton = 6;
+int rightButton = 7;
+
+// Quick note: variables aren't allowed to contain spaces.
+// Often, programmersUseCamelCaseInstead, (see how it looks like a camel?)
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  
+  
+  // Helpful hints to avoid frustrating mistakes:
+  //   * Make sure everything is spelled correctly, including capitalization
+  //   * Every line of code should have a semicolon ";" at the end
+  //   * Check the documentation to make sure each function does what you think
+  
+  
+  // ------------------- START CODING BELOW HERE -----------------------
+  
+  
+  
+}
+
+
+void loop() {}
